d53a76eb4584786dd79598ebc45ab0173a4aac30
Matt Keys <MatthewjKeys22@gmail.com>
2023-05-20 09:04:24 -0400

    aws stuff

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {ConfigService} from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService)
+  let AWS = require("aws-sdk");
+  AWS.config.update({
+    accessKeyId: configService.get('accessKeyId'),
+    secretAccessKey: configService.get('secretAccessKey'),
+    region: configService.get('region')
+  });
+
   await app.listen(3000);
 }
 bootstrap();
